8b7b7a3530b34ff39e240e3de09f50b359064f85
Nasir Ahmed <ahsannasirahmed@gmail.com>
2024-01-08 02:42:39 +0600

    3

--- js/json.js
+++ js/json.js
@@ -4,4 +4,21 @@ const stringified = JSON.stringify(user);
 console.log(stringified);
 console.log(user);
 
+const shop = {
+    owner : 'Alia',
+    addrerss : {
+        street : 'kochuhet bajar road number 07',
+        city : 'Ranbir',
+        country: 'Bangladesh'
 
+    },
+    products : [ 'Laptop' , 'mic', 'monitor', 'keyboard'],
+    revenue : 450000,
+    isOpen : true,
+    isNew : false
+}
+console.log(shop);
+const shopJson = JSON.stringify(shop);
+console.log(shopJson);
+const shopObject = JSON.parse(shopJson);
+console.log(shopObject);
