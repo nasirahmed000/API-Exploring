fb5e078aa170df899d6e1767ced83b10a035541d
Nasir Ahmed <ahsannasirahmed@gmail.com>
2024-01-08 02:17:18 +0600

    2

---
+++ js/app.js
@@ -0,0 +1,2 @@
+const user = {id: 1 , name : 'gorib Aamir ', job : 'Actor '};
+console.log(user);
---
+++ js/json.js
@@ -0,0 +1,7 @@
+const user = {id: 1 , name : 'gorib Aamir ', job : 'Actor '};
+// json , Javascript Object notation 
+const stringified = JSON.stringify(user);
+console.log(stringified);
+console.log(user);
+
+
