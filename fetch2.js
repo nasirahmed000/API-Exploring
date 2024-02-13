1444213c9fe3dec893ef080f67051396f6aec3f9
Nasir Ahmed <80189624+nasirahmed000@users.noreply.github.com>
2024-02-13 23:36:04 +0600

    Update fetch2.js

--- fetch2.js
+++ fetch2.js
@@ -1,3 +1,10 @@
 fetch('https://jsonplaceholder.typicode.com/todos/1')
 .then (res => res.json())
 .then (json => console.log(json));
+
+
+
+
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+.then (res => res.json())
+.then (json => console.log(json));
