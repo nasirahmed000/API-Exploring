8df456c34e645bb42e5fbffbfd6b1dab4c134cb5
Nasir Ahmed <80189624+nasirahmed000@users.noreply.github.com>
2024-02-23 00:59:13 +0600

    Update fetch2.js

--- fetch2.js
+++ fetch2.js
@@ -2,7 +2,14 @@ fetch('https://jsonplaceholder.typicode.com/todos/1')
 .then (res => res.json())
 .then (json => console.log(json));
 
-
+const person = {
+  firstName: "John",
+  lastName : "Doe",
+  id       : 5566,
+  fullName : function() {
+    return this.firstName + " " + this.lastName;
+  }
+};
 
 
 fetch('https://jsonplaceholder.typicode.com/todos/1')
