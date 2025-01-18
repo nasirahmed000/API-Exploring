3a1c28e508f94a41e47af33c8c82de25bf5b03af
Nasir Ahmed <80189624+nasirahmed000@users.noreply.github.com>
2025-01-18 21:03:08 +0600

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,3 +1,8 @@
 fetch('https://jsonplaceholder.typicode.com/todos/1')
 .then (res => res.json())
 .then (json => console.log(json))
+
+
+fetch('nasirhmed.com')
+.then (res=> res.json())
+.then (json=>console.log(json))
